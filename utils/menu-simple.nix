b1f7fb1a998032127b3b9fa93724b0454347c423
Rickard Nilsson <rickynils@gmail.com>
2013-02-12 12:20:54 +0100

    Use library functions in menu-simple

--- utils/menu-simple.nix
+++ utils/menu-simple.nix
@@ -1,31 +1,21 @@
 { request, ... }:
 
-with builtins;
 with import <nixpkgs> {};
+with callPackage ../lib/nixrbd.nix {};
 
-let
-
-  mkIpxe = s: writeText "script.ipxe" "#!ipxe\n${s}";
-
-  path = if request.pathInfo == [] then "" else head request.pathInfo;
-
-  handlers = {
-    memtest = mkIpxe "chain ${memtest86plus}/memtest.bin";
-
-    default = mkIpxe ''
-      :menu_root
-      menu Select a boot alternative
-      item memtest Run Memtest
-      item iso     Run Slitaz Live CD
-      item shell   Enter iPXE shell
-      choose target && goto ''${target} || goto menu_root
-
-      :shell
-      shell && goto menu_root
-
-      :memtest
-      chain /memtest
-    '';
+mkDefaultMenu request {
+  memtest = {
+    title = "Run Memtest";
+    script = "chain ${memtest86plus}/memtest.bin";
   };
 
-in if hasAttr path handlers then getAttr path handlers else handlers.default
+  nixos = {
+    title = "Run Nixos";
+    script = mkNixosBootEntry (
+      import <nixos> {
+        nixpkgs = <nixpkgs>;
+        configuration = <configuration.nix>;
+      }
+    );
+  };
+}
