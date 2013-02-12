da8c71a44b2d796c5bb3738eb0b945887fa38ee6
Rickard Nilsson <rickynils@gmail.com>
2013-02-12 17:21:35 +0100

    Install nixrbd.nix in include dir

--- default.nix
+++ default.nix
@@ -8,6 +8,10 @@ cabal.mkDerivation (self: {
   isLibrary = false;
   isExecutable = true;
   src = ./.;
+  postInstall = ''
+    mkdir -p $out/include
+    cp lib/nixrbd.nix $out/include/
+  '';
   buildDepends = [
     cmdargs
     httpTypes
