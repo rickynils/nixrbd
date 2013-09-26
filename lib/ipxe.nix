6d49cd32110436bb29602eaebb410928bac59b61
Rickard Nilsson <rickynils@gmail.com>
2013-09-26 20:23:05 +0200

    lib/ipxe: Update ipxe. Make it possible to build any ipxe artifact

--- lib/ipxe.nix
+++ lib/ipxe.nix
@@ -1,17 +1,17 @@
 { stdenv, fetchgit
 , perl, syslinux, cdrkit, which, coreutils
-, imageType ? "iso", embeddedScript ? null
+, build ? "ipxe.iso", embeddedScript ? null
 }:
 
 with stdenv.lib;
 
 stdenv.mkDerivation {
-  name = "ipxe-git.${imageType}";
+  name = "ipxe-git.${build}";
 
   src = fetchgit {
     url = "git://git.ipxe.org/ipxe.git";
-    rev = "3fcb8cf8dceb45f8b01e1d69d43cfd99df43b78a";
-    sha256 = "11b15myz7s3vgk2lja95c5cvgf2yhcjjyhy0ka2zij5zlyivjm4d";
+    rev = "7405685df2bea9a457970d8b5a63ede08fcda6f7";
+    sha256 = "0vs9js1x6ff58hw0xm7s0ym476bsvpr7g6mhg9sd65vpmdic4ysa";
   };
 
   buildInputs = [ perl syslinux cdrkit which coreutils ];
@@ -25,10 +25,10 @@ stdenv.mkDerivation {
     make \
       ${optionalString (embeddedScript != null) "EMBED=${embeddedScript}"} \
       ISOLINUX_BIN=${syslinux}/share/syslinux/isolinux.bin \
-      bin/ipxe.${imageType}
+      bin/${build}
   '';
 
   installPhase = ''
-    cp bin/ipxe.${imageType} $out
+    cp bin/${build} $out
   '';
 }
